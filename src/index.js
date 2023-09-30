56d8fa6f7ab03ad61eb6444e8298b2dd26443f69
fcoagz <grihardware@gmail.com>
2023-09-29 21:36:21 -0400

    ~

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 const { default: axios } = require("axios");
 
-// pyDolarVenezuela API by Francisco Griman
-// Documentation: https://github.com/fcoagz/api-pydolarvenezuela
-const urlBase = 'https://pydolarvenezuela-api.vercel.app/';
+const urlBase = 'https://exchange.vcoud.com/'; // Hello World xD
 
-/**
- * Obtener contenido de la página.
- * 
- * @param {string} url - Page URL
- * @returns {Promise<any>}
- */
-async function getContentPage(url) {
-    try {
-        const response = await axios.get(url);
-        if (response.status !== 200) {
-            throw new Error(`Error de comunicación pyDolarVenezuela-API. Codigo: ${response.status}`);
-        }
-
-        return response.data;
-    } catch (error) {
-        console.error(`ValueError: ${error.message}`);
+function _convert_specific_format(text, character = '-') {
+    const acentos = { 'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u' };
+    for (const [acento, sin_acento] of Object.entries(acentos)) {
+        text = text.toLowerCase().replace(acento, sin_acento).replace(' ', character);
     }
+    return text;
+}
+
+function _convert_dollar_name_to_monitor_name(monitor_name) {
+    if (monitor_name.split(' ')[0] === "Dólar" && monitor_name !== "Dólar Today") {
+        if (monitor_name === "Dólar Monitor") {
+            return "EnParaleloVzla";
+        } else {
+            return monitor_name.split(' ')[1];
+        }
+    }
+    return monitor_name;
 }
 
 /**
@@ -32,51 +30,52 @@ async function getContentPage(url) {
  * @returns {Promise<string|object>} Un objeto con la información solicitada o una cadena de texto en caso de formateo especial.
  */
 async function getMonitor(monitorCode, nameProperty, prettify) {
-    const response = await getContentPage(urlBase + 'api/v1/dollar/');
-    const allMonitors = response['monitors'];
-
-    if (!(monitorCode in allMonitors)) {
-        return allMonitors;
-    }
-
+    let data = {};
+    const response = await axios.get(urlBase + 'coins/latest');
     try {
-        const monitorData = allMonitors[monitorCode.toLowerCase()];
-        
-        if (nameProperty) {
-            const value = monitorData[nameProperty];
-            if (value === undefined) {
-                throw new Error("Consulte la documentación de la biblioteca: https://github.com/fcoagz/consulta-dolar-venezuela");
-            } else {
-                return nameProperty === 'price' && prettify ? `Bs. ${value}` : value;
-            }
-        } else {
-            return monitorData;
+        if (response.status !== 200) {
+            throw new Error(`Error de comunicación CriptoDolar. Codigo: ${response.status}`);
         }
-    } catch (error) {
-        console.error(`KeyError: ${error.message}`);
+
+        const jsonResponse = response.data;
+
+        for (const monitor of jsonResponse) {
+            if (monitor.type.includes('bolivar') || monitor.type.includes('bancove')) {
+                const result = {
+                    title: _convert_dollar_name_to_monitor_name(monitor.name),
+                    price: monitor.price,
+                    price_old: monitor.priceOld,
+                    type: monitor.type === 'bancove' ? 'bank' : 'monitor',
+                    lastUpdate: monitor.updatedAt
+                };
+
+                data[_convert_specific_format(result.title)] = result;
+            }
+        }
+        if (!(monitorCode in data)) {
+            return data;
+        }
+
+        try {
+            const monitorData = data[monitorCode.toLowerCase()];
+            
+            if (nameProperty) {
+                const value = monitorData[nameProperty];
+                if (value === undefined) {
+                    throw new Error("Consulte la documentación de la biblioteca: https://github.com/fcoagz/consulta-dolar-venezuela");
+                } else {
+                    return nameProperty === 'price' && prettify ? `Bs. ${value}` : value;
+                }
+            } else {
+                return monitorData;
+            }
+        } catch (error) {
+            console.error(`KeyError: ${error.message}`);
+        }
+   
+    } catch (e) {
+        console.error(e);
     }
 }
 
-/**
- * Bbtener la hora actual en Venezuela.
- *
- * @param {string} dateOrTime - La propiedad específica que se desea obtener del tiempo `date` || `time`. 
- * @returns {Promise<string|object>} Un objeto con la información solicitada o una cadena de texto en caso de formateo especial.
- */
-async function getDate(dateOrTime) {
-    try {
-    const response = await getContentPage(urlBase + 'api/v1/dollar/fecha');
-    
-     if (dateOrTime === 'all') {
-         return response['datetime'];
-     } else if (dateOrTime in response['datetime']) {
-         return response['datetime'][dateOrTime];
-     } else {
-         throw new Error("Consulte la documentación de la biblioteca: https://github.com/fcoagz/consulta-dolar-venezuela");
-     }
-    } catch (error) {
-    console.error(`KeyError: ${error.message}`);
-    }
-}
-
-module.exports = { getMonitor, getDate }
+module.exports = { getMonitor };
